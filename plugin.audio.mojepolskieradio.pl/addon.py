b7ee9decd804fad43f16b90e005e4293c413e10d
smuto.promyk@gmail.com <smuto.promyk@gmail.com@d5639b79-a202-f6e3-3094-b50fe9276780>
2012-02-14 20:09:21 +0000

    [mojepolskieradio.pl] - następna niepotrzebna rzecz, w ustawieniach wybór rodzaju strumienia

--- plugin.audio.mojepolskieradio.pl/addon.py
+++ plugin.audio.mojepolskieradio.pl/addon.py
@@ -1,4 +1,6 @@
 ﻿# -*- coding: utf-8 -*-
+import xbmcaddon
+
 import sys
 import simplejson
 import urllib2
@@ -6,34 +8,45 @@ import urllib2
 import xbmcgui
 import xbmcplugin
 
+__settings__ = xbmcaddon.Addon(id='plugin.audio.mojepolskieradio.pl')
+__language__ = __settings__.getLocalizedString
+
 CHANNELS_URL = 'http://moje.polskieradio.pl/api/?key=20439fdf-be66-4852-9ded-1476873cfa22&output=json'
 
 def showChannels():
     u = urllib2.urlopen(CHANNELS_URL)
     data = u.read()
+    json = simplejson.loads(data)
     u.close()
-
-    channels = simplejson.loads(data[11:-1])
-
+    channels = json['channel']
     for channel in channels:
-        
         xbmcplugin.setContent(HANDLE, 'albums')
-        
         item = xbmcgui.ListItem(channel['title'], iconImage = channel['image'], thumbnailImage = channel['image'])
         item.setProperty('IsPlayable', 'true')
         item.setProperty("IsLive", "true")
-        item.setProperty('Album_Description', channel['description'])
+        item.setProperty('Album_Description', channel['description'].replace('\n',' '))
         item.setInfo( type="Music",  infoLabels = {
-                'title' : channel['title'] ,
-                'artist' : channel['category'] ,
-                'album' : 'moje.polskieradio.pl' ,
+                'title' : 'PolskieRadio' ,
+                'artist' : 'moje.polskieradio.pl' ,
+                'year': 2012 ,
+                'genre': channel['category'] ,
+                'album' : channel['title'] ,
                 'comment' : channel['description']
         })
-        for i in range(len(channel["AlternateStationsStreams"])):
-          if "RTSP" == channel["AlternateStationsStreams"][i]['name'] :
-              rtsp = channel["AlternateStationsStreams"][i]['link'] 
-
-        xbmcplugin.addDirectoryItem(HANDLE, str(rtsp).replace('rtsp://','rtmp://'), item)
+        streams = channel["AlternateStationsStreams"]
+        xbmc_streams = []
+        for stream in streams:
+            if stream['name'] == "mp3":
+                link = stream['link']
+                xbmc_streams.append(link)
+            if  stream['name'] == "RTMP":
+                link = stream['link']
+                xbmc_streams.append(link)
+        if __settings__.getSetting('stream') == "mp3":
+            url = xbmc_streams[0]
+        else:
+            url = xbmc_streams[-1]
+        xbmcplugin.addDirectoryItem(HANDLE, url, item)
 
     xbmcplugin.endOfDirectory(HANDLE)
 
