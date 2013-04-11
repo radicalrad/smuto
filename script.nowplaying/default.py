ef2bb80e771bf2f7b1ad20ee80135ae0aeb86116
smuto.promyk@gmail.com <smuto.promyk@gmail.com@d5639b79-a202-f6e3-3094-b50fe9276780>
2013-04-11 12:02:04 +0000

    [nowplaying] - init

---
+++ script.nowplaying/default.py
@@ -0,0 +1,30 @@
+import xbmcaddon, xbmc, xbmcgui
+
+__settings__ = xbmcaddon.Addon(id='script.nowplaying')
+__language__ = __settings__.getLocalizedString
+__cwd__      = __settings__.getAddonInfo('path')
+__icon__      = __settings__.getAddonInfo('icon')
+
+class showInfo(xbmcgui.WindowXMLDialog):
+
+    def __init__(self, *args, **kwargs):
+        xbmcgui.WindowXMLDialog.__init__(self, *args, **kwargs)
+
+    def onInit(self):
+        self.defineControls()
+    def defineControls(self):
+        self.action_cancel_dialog = (9, 10)
+    def closeDialog(self):
+        self.close()
+    def onAction(self, action):
+        if action in self.action_cancel_dialog:
+            self.closeDialog()
+
+if xbmc.Player().isPlayingAudio():
+    w = showInfo("script-nowplaying.xml", __cwd__)
+    w.doModal()
+    del w
+else:
+    xbmc.executebuiltin("XBMC.Notification("+__language__(30000)+","+__language__(30001)+",5000,"+__icon__+")")
+
+
